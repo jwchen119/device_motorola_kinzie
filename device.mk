c3723d098496af775db1a38d22c2222a4a054050
Jorge Martin <angel.arasthel@gmail.com>
2016-02-26 13:15:35 +0100

    Fix frozen charging screen when charging a powered-off device
    Change-Id: Idb8296a1b220476a3d100ebc58bebb0b2975d65b

--- device.mk
+++ device.mk
@@ -35,7 +35,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-kinzie.conf:system/etc/thermal-engine-kinzie.conf
+    $(LOCAL_PATH)/configs/thermal-engine-kinzie.conf:system/etc/thermal-engine-kinzie.conf \
+$(LOCAL_PATH)/configs/thermal-engine-kinzie.conf:system/etc/thermal-engine-kinzie2pc.conf \
+$(LOCAL_PATH)/configs/thermal-engine-kinzie.conf:system/etc/thermal-engine-kinziep3.conf 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml \
